a1476d4312e47a174bc9498f0e77793865539161
Daniel Melchizedek Arockia Thomas Samuel <31015432+Daniel-Melchizedek@users.noreply.github.com>
2026-03-08 18:49:04 +0530

    microsoft applied skills - csharp basics

---
+++ Classes_M1/BankCustomer.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Classes_M1;
+
+public class BankCustomer
+{
+    private static int s_nextCustomerId;
+    
+    public string FirstName = "Tim";
+    public string LastName = "Shao";
+    public readonly string CustomerId;
+
+    static BankCustomer()
+    {
+        Random random = new Random();
+        s_nextCustomerId = random.Next(1000000000, 2000000000);
+    }
+
+    public BankCustomer()
+    {
+        this.CustomerId = (s_nextCustomerId++).ToString("D10");
+    }
+    public BankCustomer(string firstName, string lastName):this()
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    
+}
